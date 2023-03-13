295e99f6a79ff70ccd3d0feeb4724b6a70c3a04b
haimgetanech <haimgetanech@gmail.com>
2023-03-13 13:37:33 +0200

    change

--- src/targil12.java
+++ src/targil12.java
@@ -1,36 +1,41 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class targil12 {
     public static void main(String[] args) {
 //        int[] arr1 = {10, 20, 30, 40, 50};
 //        System.out.println(arr(arr1, 80));
 
-        int[] arr1={1,3,5};
-        System.out.println(array(arr1,3));
+        int[] arr1 = {1, 3, 5};
+        System.out.println(Arrays.toString(array(arr1, 3)));
 
-        }
+    }
 
     public static boolean arr(int[] array1, int n) {
-       int sum=0;
-       int sum2 =0;
+        int sum = 0;
+        int sum2 = 0;
         for (int i = 0; i < array1.length; i++) {
             for (int j = 0; j < array1.length; j++) {
-                if(array1[i]+array1[j]==n){
+                if (array1[i] + array1[j] == n) {
                     return true;
+                }
             }
         }
-    }return false;
-}
-   public static int[] array (int[] arr,int k){
-        int[] newarr = new int[arr.length*k];
-       int index = 0;
-       for (int i = 0; i < arr.length; i++) {
-           for (int j = 0; j < k; j++) {
-               newarr[index]=arr[i];
-               index++;
-           }
-       }return newarr;
-   }
+        return false;
+    }
+
+    public static int[] array(int[] arr, int k) {
+        int[] newArr = new int[arr.length * k];
+        int index = 0;
+        int count = 0;
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = arr[index];
+            count++;
+            if (count == k) {
+                count = 0;
+                index++;
+            }
+        }
+        return newArr;
+    }
 
 }
